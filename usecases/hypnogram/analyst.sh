0b6a9a67b76b13af61f852c78c8fff366f5926fb
bowden <james.bowden@med.uni-goettingen.de>
2025-09-24 14:33:32 +0200

    Fix analyst test script

--- usecases/hypnogram/analyst.sh
+++ usecases/hypnogram/analyst.sh
@@ -2,4 +2,4 @@
 HOST=$1
 SCAN=$2
 QUERY=$3
-./analyst ${HOST} ${SCAN} ${QUERY} output/${SCAN}_${QUERY}.txt && python gui/utils/plot_hypnogram2.py output/${SCAN}_${QUERY}.txt
+./analyst ${HOST} ${SCAN} ${QUERY} output/${SCAN}_${QUERY}.txt && python gui/utils/plot_hypnogram.py output/${SCAN}_${QUERY}.txt
